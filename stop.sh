31eacea09788acf8f99ff1e9fbdf8a0d4d38b9fb
FH3095 <FH3095@users.noreply.github.com>
2025-12-23 00:06:51 +0100

    Improve stop.sh to work with systemd

--- stop.sh
+++ stop.sh
@@ -3,4 +3,11 @@
 set -u
 
 OWN_DIR=$(dirname "$0")
+BOT_PID=$(cat "$OWN_DIR/bot.pid")
 rm "$OWN_DIR/bot.pid"
+for _ in {1..15}; do
+  if ! ps -p "$BOT_PID" >/dev/null; then
+    exit 0
+  fi
+  sleep 1
+done
